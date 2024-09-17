94dba3cdfa12f6f6f53df74fb90e8f66c1f53e70
Viktor Morhun <morhun.fe@gmail.com>
2024-09-17 16:20:34 +0300

    update pagination

--- domApp.js
+++ domApp.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 import { fetchData } from "./fetchData.js";
 import { apikey, FETCH_BASE_URL } from "./constants.js";
@@ -8,7 +8,7 @@ class domApp {
     this.moviesData = [];
     this.searchResult = [];
     this.containerId = containerId;
-    this.searchQuerry = '';
+    this.searchQuerry = "";
     this.currentPage = 1;
     this.previousPage = 1;
     this.totalResults = 0;
@@ -21,16 +21,20 @@ class domApp {
     // this.renderMoviesList();
     this.renderMessage();
     this.addEventListeners();
-  }
+  };
 
   renderMoviesList(moviesList = this.moviesData) {
     const containerElement = document.getElementById(this.containerId);
-    containerElement.innerHTML = moviesList.map((movie) => this.renderMovieCard(movie)).join('');
+    containerElement.innerHTML = moviesList
+      .map((movie) => this.renderMovieCard(movie))
+      .join("");
   }
 
-  renderMovieCard({Poster, Title, Year, imdbID}) {
+  renderMovieCard({ Poster, Title, Year, imdbID }) {
     return `<div class="movie-card">
-                <img src="${Poster === 'N/A' ? 'no-image-icon.png' : Poster}" alt="Movie Poster" class="movie-card__image">
+                <img src="${
+                  Poster === "N/A" ? "no-image-icon.png" : Poster
+                }" alt="Movie Poster" class="movie-card__image">
                 <div class="movie-card__overlay">
                     <h3 class="movie-card__title">${Title}</h3>
                     <p class="movie-card__year">${Year}</p>
@@ -41,83 +45,97 @@ class domApp {
 
   renderMessage(message = `Search movie to start!`) {
     const containerElement = document.getElementById(this.containerId);
-    containerElement.innerHTML = `<h3 class="movie-carousel__message">${message}</h3>`
+    containerElement.innerHTML = `<h3 class="movie-carousel__message">${message}</h3>`;
   }
 
   searchMovies() {
     event.preventDefault();
-    const searchBarElement = document.getElementById('movie-title');
+    const searchBarElement = document.getElementById("movie-title");
     this.searchQuerry = searchBarElement.value;
     this.getMoviesByQuerry(this.searchQuerry);
   }
   getMoviesByQuerry = async (querry = this.searchQuerry) => {
-    const resultOfSearch = await fetchData(`${FETCH_BASE_URL}${querry}${apikey}&page=${this.currentPage}`);
-    console.log(resultOfSearch);
-
+    const resultOfSearch = await fetchData(
+      `${FETCH_BASE_URL}${querry}${apikey}&page=${this.currentPage}`
+    );
     if (resultOfSearch.Error) {
       this.renderMessage(`Movie ${querry} not found!`);
     } else {
       this.searchResult = resultOfSearch.Search;
       this.renderMoviesList(this.searchResult);
-      
-      if(resultOfSearch.totalResults > 10) {
-        this.totalResults = Math.ceil(resultOfSearch.totalResults/10);
-        console.log('pagination', this.totalResults);
+
+      if (resultOfSearch.totalResults > 10) {
+        this.totalResults = Math.ceil(resultOfSearch.totalResults / 10);
         this.renderPagination();
       }
     }
-
-  }
+  };
   addEventListeners() {
-    const searchButtonElement = document.getElementById('movie-search-button');
+    const searchButtonElement = document.getElementById("movie-search-button");
 
-    searchButtonElement.addEventListener('click', () => this.searchMovies());
+    searchButtonElement.addEventListener("click", () => this.searchMovies());
   }
-
 
   //pagination
   renderPagination() {
-    const paginationElement = document.getElementById('pagination-container');
-    
+    const paginationElement = document.getElementById("pagination-container");
 
     paginationElement.innerHTML = `
         <div class="pagination-container">
+          <div class="pagination-number arrow" id="firstPage">
+            <span class="material-symbols-outlined">
+              keyboard_double_arrow_left
+            </span>
+              <span class="arrow-text">First</span> 
+          </div>
             <div class="pagination-number arrow" id="previousPage">
                 <span class="material-symbols-outlined">
                   chevron_left
                 </span>
-              <span class="arrow-text">Previous</span> 
             </div>
 
             <input type="number" class="pagination-input" id="currentPage" min="1" max="${this.totalResults}" value=${this.currentPage}>
-
+            <span class="pagination-number pagination-number-total">of ${this.totalResults}</span>
             <div class="pagination-number arrow" >
+
                 <span class="material-symbols-outlined" id="nextPage">
-                    chevron_right
+                  chevron_right
                 </span>
             </div>
+              <div class="pagination-number arrow" id="lastPage">
+                <span class="arrow-text">Last</span> 
+
+                <span class="material-symbols-outlined">
+                  keyboard_double_arrow_right
+                </span>
+              </div>
           </div>`;
-    
-    const pageNumberElement = document.getElementById('currentPage');
-    pageNumberElement.removeEventListener('change', this.setPaginationPage);
-    pageNumberElement.addEventListener('change', this.setPaginationPage);
-    paginationElement.removeEventListener('click', this.changePaginationPage);
-    paginationElement.addEventListener('click', this.changePaginationPage);
+
+    const pageNumberElement = document.getElementById("currentPage");
+    pageNumberElement.removeEventListener("change", this.setPaginationPage);
+    pageNumberElement.addEventListener("change", this.setPaginationPage);
+    paginationElement.removeEventListener("click", this.changePaginationPage);
+    paginationElement.addEventListener("click", this.changePaginationPage);
   }
 
-  changePaginationPage = () => {
-    if (event.target.closest('#previousPage')) {
-      console.log(this.currentPage);
-      this.currentPage = (this.currentPage - 1 === 0) ? 1 : --this.currentPage;
-      console.log(this.currentPage);
+  changePaginationPage = (event) => {
+    if (event.target.closest("#previousPage")) {
+      this.currentPage = this.currentPage - 1 === 0 ? 1 : --this.currentPage;
       this.getMoviesByQuerry();
-    } else if (event.target.id === 'nextPage') {
-      this.currentPage = (this.currentPage === this.totalResults) 
-        ? this.currentPage 
-        : ++this.currentPage;
+    } else if (event.target.id === "nextPage") {
+      this.currentPage =
+        this.currentPage === this.totalResults
+          ? this.currentPage
+          : ++this.currentPage;
+      this.getMoviesByQuerry();
+    } else if (event.target.closest("#firstPage")) {
+      this.currentPage = 1;
+      this.getMoviesByQuerry();
+    } else if (event.target.closest("#lastPage")) {
+      this.currentPage = this.totalResults;
       this.getMoviesByQuerry();
     }
-  }
+  };
 
   setPaginationPage = () => {
     const inputPageNumber = Number(event.target.value);
@@ -128,10 +146,11 @@ class domApp {
     } else {
       this.currentPage = inputPageNumber;
     }
-      this.getMoviesByQuerry();
-  }
+
+    this.getMoviesByQuerry();
+  };
 }
 
-const cinema = new domApp('results-container');
+const cinema = new domApp("results-container");
 
- cinema.setDefaultMovies();
+cinema.setDefaultMovies();
